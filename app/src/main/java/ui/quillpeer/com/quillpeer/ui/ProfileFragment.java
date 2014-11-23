e30e63572152098fa1222a2d035c949161518b08
loucas <loukkis91@gmail.com>
2014-11-23 13:55:21 +0000

    23/11/2014 14:00 -Developed a new xml file for profile (fragment_profile_cardview) -Added two new dependencies in app Build grandle

--- app/src/main/java/ui/quillpeer/com/quillpeer/ui/ProfileFragment.java
+++ app/src/main/java/ui/quillpeer/com/quillpeer/ui/ProfileFragment.java
@@ -19,7 +19,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_profile_cardview, container, false);
 
 
         return rootView;
