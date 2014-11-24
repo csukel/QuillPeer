2f4c78fa22d25f4c85b55250c7f9664036471a64
loucas <loukkis91@gmail.com>
2014-11-24 08:45:57 +0000

    24/11/2014 08:45 -AllPeopleAdapter is completed . -AllPeople fragment needs some design changes

--- app/src/main/java/ui/quillpeer/com/quillpeer/ui/people/AllFragment.java
+++ app/src/main/java/ui/quillpeer/com/quillpeer/ui/people/AllFragment.java
@@ -3,21 +3,49 @@ package ui.quillpeer.com.quillpeer.ui.people;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import core.Person;
 import ui.quillpeer.com.quillpeer.R;
 
 /**
  * Created by loucas on 18/11/2014.
  */
 public class AllFragment extends Fragment {
+    private List<Person> peopleList;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_people_all, container, false);
-
+        RecyclerView recList = (RecyclerView) rootView.findViewById(R.id.cardListPeople);
+        //recList.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(rootView.getContext());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recList.setLayoutManager(llm);
+        populateList();
+        AllPeopleAdapter allPeopleAdapter = new AllPeopleAdapter(peopleList);
+        recList.setAdapter(allPeopleAdapter);
         return rootView;
+    }
+
+    public void populateList(){
+        Person person1 = new Person("Mr","Loucas","Stylianou","University of Warwick","Department of Computer Science");
+        Person person2 = new Person("Mr","Hao","Dong","University of Warwick","Department of Computer Science");
+        Person person3 = new Person("Mr","Marios","Chrysanthou","University of Warwick","Department of Computer Science");
+        Person person4 = new Person("Dr","Maria","Liakat","University of Warwick","Department of Computer Science");
+        Person person5 = new Person("Dr","Matt","Leeke","University of Warwick","Department of Computer Science");
+        peopleList = new ArrayList<Person>();
+        peopleList.add(person1);
+        peopleList.add(person2);
+        peopleList.add(person3);
+        peopleList.add(person4);
+        peopleList.add(person5);
     }
 }
--- app/src/main/java/ui/quillpeer/com/quillpeer/ui/people/AllPeopleAdapter.java
+++ app/src/main/java/ui/quillpeer/com/quillpeer/ui/people/AllPeopleAdapter.java
@@ -1,17 +1,32 @@
 package ui.quillpeer.com.quillpeer.ui.people;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
 import core.Person;
+import ui.quillpeer.com.quillpeer.R;
 
 /**
  * Created by loucas on 23/11/2014.
  */
-public class AllPeopleAdapter extends RecyclerView.Adapter<AllPeopleViewHolder> {
+public class AllPeopleAdapter extends RecyclerView.Adapter<AllPeopleAdapter.AllPeopleViewHolder> {
 
     private List<Person> personList;
+
+    @Override
+    public AllPeopleViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.
+                from(viewGroup.getContext()).
+                inflate(R.layout.card_list_people_item, viewGroup, false);
+
+        return new AllPeopleViewHolder(itemView);
+    }
 
     public AllPeopleAdapter(List<Person> personList){
         this.personList = personList;
@@ -20,5 +35,34 @@ public class AllPeopleAdapter extends RecyclerView.Adapter<AllPeopleViewHolder> 
     @Override
     public int getItemCount() {
         return personList.size();
+    }
+
+
+    @Override
+    public void onBindViewHolder(AllPeopleViewHolder peopleViewHolder, int i) {
+        Person person = personList.get(i);
+        peopleViewHolder.txtPeoplePersonalDetails.setText(person.getTitle() + " " + person.getName() + " " + person.getSurname());
+        peopleViewHolder.txtPeopleUniversity.setText(person.getUniversity());
+        peopleViewHolder.txtPeopleDepartment.setText(person.getDepartment());
+
+    }
+
+
+    public static class AllPeopleViewHolder extends RecyclerView.ViewHolder {
+        protected ImageView imgPeopleProfilePic;
+        protected TextView txtPeoplePersonalDetails;
+        protected TextView txtPeopleUniversity;
+        protected TextView txtPeopleDepartment;
+        protected ImageView imgPeopleFavourite;
+
+        public AllPeopleViewHolder(View v){
+            super(v);
+            imgPeopleFavourite = (ImageView)v.findViewById(R.id.imgPeopleFavourite);
+            imgPeopleProfilePic = (ImageView)v.findViewById(R.id.imgPeopleProfilePic);
+            txtPeoplePersonalDetails = (TextView)v.findViewById(R.id.txtPeoplePersonalDetails);
+            txtPeopleDepartment = (TextView)v.findViewById(R.id.txtPeopleDepartment);
+            txtPeopleUniversity = (TextView)v.findViewById(R.id.txtPeopleUniversity);
+
+        }
     }
 }
