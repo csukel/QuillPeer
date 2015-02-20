1a7870a32f056c16cb3714b3d7898ff5b886f5e4
loucas <loukki91@gmail.com>
2015-02-20 20:59:52 +0000

    20/2/2014 20:59 -TutorialActivity additions

--- app/src/main/java/ui/quillpeer/com/quillpeer/ui/TutorialActivity.java
+++ app/src/main/java/ui/quillpeer/com/quillpeer/ui/TutorialActivity.java
@@ -1,14 +1,13 @@
 package ui.quillpeer.com.quillpeer.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-
+import android.view.Menu;
+import android.view.MenuItem;
 import com.viewpagerindicator.CirclePageIndicator;
 import com.viewpagerindicator.PageIndicator;
-import com.viewpagerindicator.TitlePageIndicator;
-import com.viewpagerindicator.UnderlinePageIndicator;
-
 import ui.quillpeer.com.quillpeer.R;
 
 /**
@@ -18,18 +17,75 @@ public class TutorialActivity extends FragmentActivity {
     private ViewPager mPager;
     private TutorialPageAdapter mAdapter;
     private PageIndicator mIndicator;
+    private MenuItem skipTutorial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //set the screen UI
         setContentView(R.layout.tutorial);
-
+        //instantiate the page adapter
         mAdapter = new TutorialPageAdapter(getSupportFragmentManager());
-
+        //instantiate the view pager
         mPager = (ViewPager)findViewById(R.id.tutorial_pager);
+        //set the adapter to the view pager
         mPager.setAdapter(mAdapter);
-
+        //set and indicator with circles at the bottom of the page
         mIndicator = (CirclePageIndicator)findViewById(R.id.indicator);
+        //set a page change listener
+        mIndicator.setOnPageChangeListener(pageChangeListener);
+        //bind the indicator to the pager
         mIndicator.setViewPager(mPager);
+    }
+
+    //instantiate a page change listener
+    ViewPager.OnPageChangeListener pageChangeListener = new ViewPager.OnPageChangeListener() {
+        //when a page is scrolled then do
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            //if the visible page is the last one ...
+            if (position == mAdapter.getCount()-1){
+                //change the skip item title to proceed
+                skipTutorial.setTitle("Proceed");
+            }
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //get the menu inflater and connect it to the corresponding menu xml file(tutorial.xml under the menu)
+        getMenuInflater().inflate(R.menu.tutorial, menu);
+        //instantiate the skip menu item
+        skipTutorial = menu.findItem(R.id.action_skip);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //when a menu item is selected do...
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            //if skip menu item is pressed do ..
+            case R.id.action_skip:
+                //TODO if last activity was the splash screen then by pressing skip proceed to login screen
+                Intent prevIntent = getIntent();
+                if (prevIntent.getStringExtra("class")!=null && prevIntent.getStringExtra("class").equals("SplashActivity")){
+                    Intent intent = new Intent(this,LoginActivity.class);
+                    startActivity(intent);
+                }
+
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
